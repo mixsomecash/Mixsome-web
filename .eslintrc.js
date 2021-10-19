b0125019f6a099559ff3b4cdf8a9b229b342db6f
mixsomecash <80465830+mixsomecash@users.noreply.github.com>
2021-10-20 01:49:09 +0400

    Adding formats

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'react/self-closing-comp': 0,
+    'react/require-default-props': 0,
+    'import/prefer-default-export': 0,
+    'no-debugger': 0,
+  },
+}
