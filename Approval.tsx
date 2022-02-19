7320d3874973e6a35a0e4f9da36647b6f5a73089
samar heydari <99286987+samarheydari01@users.noreply.github.com>
2022-02-20 00:41:18 +0330

    Add files via upload
    Approval test

---
+++ Approval.tsx
@@ -0,0 +1,97 @@
+import { ErrorMessage, Loader } from 'components'
+import React, { useEffect, useState } from 'react'
+import { useMoralis } from 'react-moralis'
+import { ChainId } from 'types/moralis'
+import { getEllipsisText } from 'utils/formatters'
+import { getApprovals, revoke } from './ApprovalHelp'
+import { ApprovalTransactions } from './types'
+
+const Approval = () => {
+  const { chainId, account } = useMoralis()
+  const [isLoading, setIsLoading] = useState(true)
+  const [approvals, setApprovals] = useState<ApprovalTransactions[]>([])
+
+  const columns = [
+    {
+      title: 'Contract',
+      render: (transaction: ApprovalTransactions) => getEllipsisText(transaction.contractAddress),
+    },
+    {
+      title: 'Date',
+      render: (transaction: ApprovalTransactions) => new Date(transaction.timestamp).toUTCString(),
+    },
+    {
+      title: 'Approved Amount',
+      render: (transaction: ApprovalTransactions) => <>{transaction.allowance}</>,
+    },
+    {
+      title: 'Revoke',
+      render: (transaction: ApprovalTransactions) => (
+        // eslint-disable-next-line react/button-has-type
+        <button
+          className="text-black bg-extra-light  hover:bg-light  hover:text-white font-bold py-2 px-4 rounded-full"
+          onClick={() => {
+            revoke(account, transaction.contractAddress, chainId as ChainId)
+          }}
+        >
+          Revoke
+        </button>
+      ),
+    },
+  ]
+
+  useEffect(() => {
+    ;(async () => {
+      if (!account || !chainId) {
+        setIsLoading(false)
+        return
+      }
+      const result = await getApprovals(account, chainId as ChainId)
+      setIsLoading(false)
+      if (result) {
+        setApprovals(result)
+      }
+    })()
+  }, [account, chainId])
+
+  return (
+    <div className="w-full bg-white px-6 py-4 xl:px-12 xl:py-10">
+      <div className="pb-10 xl:flex items-center">
+        <p className="font-medium text-20 leading-26 my-4 xl:my-0 xl:text-32 xl:leading-42">
+          Approvals
+        </p>
+      </div>
+      {isLoading && <Loader />}
+      {!isLoading && approvals && approvals.length > 0 && (
+        <div className="scrollable-table-wrapper">
+          <table className="table-auto w-full xl:mt-4">
+            <thead className="border-b border-black border-opacity-20 pb-10">
+              <tr>
+                {columns.map(column => (
+                  <td key={column.title} className="pr-4">
+                    <span className="opacity-60 text-14 xl:text-16">{column.title}</span>
+                  </td>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {approvals.map(transaction => (
+                <tr key={`${transaction.transactionHash}`}>
+                  {columns.map(column => (
+                    <td key={column.title}>{column.render(transaction)}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {!isLoading && account && approvals.length === 0 && (
+        <ErrorMessage message="No approved contracts found for the address" />
+      )}
+      {!isLoading && !account && <ErrorMessage message="Please connect your wallet" />}
+    </div>
+  )
+}
+
+export default Approval
---
+++ Dashboard.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react'
+
+import { Tabs, ToggleButton } from 'components'
+
+import Portfolio from './Portfolio'
+import NFTs from './NFTs'
+import History from './History'
+import Defi from './Defi'
+import Transfer from './Transfer'
+import Approval from './Approval'
+import { DashboardTab, DashboardNetwork } from './types'
+
+const tabs = [
+  {
+    id: DashboardTab.Portfolio,
+    title: 'Portfolio',
+  },
+  {
+    id: DashboardTab.NFTs,
+    title: 'NFTs',
+  },
+  {
+    id: DashboardTab.History,
+    title: 'History',
+  },
+  {
+    id: DashboardTab.Defi,
+    title: 'DeFi',
+  },
+  {
+    id: DashboardTab.Wallet,
+    title: 'Wallet',
+  },
+  {
+    id: DashboardTab.Approval,
+    title: 'Approval',
+  },
+]
+
+const networks = [
+  {
+    id: DashboardNetwork.Ethereum,
+    value: 'Ethereum',
+    prefix: <img className="w-6 h-6 xl:w-9 xl:h-9" src="/images/currencies/ether.png" alt="" />,
+  },
+  {
+    id: DashboardNetwork.Mixsome,
+    value: 'Mixsome',
+    prefix: <img className="w-6 h-6 xl:w-9 xl:h-9" src="/images/currencies/mixsome.png" alt="" />,
+  },
+]
+
+const Dashboard: React.FC = () => {
+  const [activeTabId, setActiveTabId] = useState(DashboardTab.Portfolio)
+  const [selectedNetworkId, setSelectedNetworkId] = useState(DashboardNetwork.Ethereum)
+  const [netWorth, setNetWorth] = useState<number | string | null>(null)
+
+  const handleTabClick = (id: DashboardTab) => setActiveTabId(id)
+
+  const handleNetworkChange = (id: DashboardNetwork) => setSelectedNetworkId(id)
+
+  const renderNetWorth = () => {
+    if (netWorth) {
+      return (
+        <div className="mb-8 xl:mb-10">
+          <div className="text-14 leading-14 font-sans xl:text-16 xl:leading-20 mb-4 opacity-60">
+            Net worth
+          </div>
+          <div className="text-20 leading-14 font-mono xl:text-22 xl:leading-20">${netWorth}</div>
+        </div>
+      )
+    }
+    return null
+  }
+
+  const renderNetwork = () => {
+    return (
+      <div className="mb-8 xl:mb-10">
+        <div className="text-14 leading-14 font-sans xl:text-16 xl:leading-20 mb-4 opacity-60">
+          Select network
+        </div>
+        <ToggleButton
+          activeId={selectedNetworkId}
+          options={networks}
+          onChange={handleNetworkChange}
+        />
+      </div>
+    )
+  }
+
+  const renderTabContent = () => {
+    switch (activeTabId) {
+      case DashboardTab.Portfolio:
+        return <Portfolio onNetWorthChange={setNetWorth} />
+      case DashboardTab.NFTs:
+        return <NFTs networkId={selectedNetworkId} />
+      case DashboardTab.History:
+        return <History />
+      case DashboardTab.Wallet:
+        return <Transfer />
+      case DashboardTab.Defi:
+        return <Defi />
+      case DashboardTab.Approval:
+        return <Approval />
+      default:
+        return null
+    }
+  }
+
+  const renderTabs = () => {
+    return (
+      <>
+        <Tabs activeTabId={activeTabId} items={tabs} onClick={handleTabClick} />
+        <div className="my-10">{renderTabContent()}</div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      {renderNetWorth()}
+      {renderTabs()}
+    </>
+  )
+}
+
+export default Dashboard
