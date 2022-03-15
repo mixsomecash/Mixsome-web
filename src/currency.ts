e065a8077c18c424162bb2b8cb0ff45e7df6067e
samar heydari <99286987+samarheydari01@users.noreply.github.com>
2022-03-15 03:32:40 +0330

    Add files via upload

---
+++ src/currency.ts
@@ -0,0 +1,15 @@
+export enum Currency {
+  Compound,
+  Binance,
+  CompoundEth,
+  CompoundDai,
+  DAI,
+  Ether,
+  MixsomeCoin,
+  Uniswap,
+  USDC,
+  YCurve,
+  Launchpool,
+  Polygon,
+  Avalanche
+}
