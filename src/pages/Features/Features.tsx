ef10dc07116b5f88b117fe8178e4050348d8587d
so-steve <s.snygin@gmail.com>
2022-03-22 15:53:34 +0400

    add all pages

---
+++ src/pages/Features/Features.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div className="w-full font-mono text-center">
+      Sorry page you are looking for does not exist :(
+    </div>
+  )
+}
+
+export default Features
