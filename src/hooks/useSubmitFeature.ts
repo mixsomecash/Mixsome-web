a89afe61e80cddb66b3f6c63e8105af2554fbdd4
so-steve <s.snygin@gmail.com>
2022-04-19 15:50:05 +0400

    hook added to submit

---
+++ src/hooks/useSubmitFeature.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { useMoralis } from 'react-moralis'
+
+type TokenDetails = {
+  name: string
+  symbol: string
+}
+
+export const useSubmitFeature = () => {
+  const { Moralis, account } = useMoralis()
+
+  const approve = async (title: string, description: string) => {
+    if (account != null) {
+      const FeatureObject = await Moralis.Object.extend('Feature')
+      const feature = new FeatureObject()
+      feature.set('title', title)
+      feature.set('description', description)
+      feature.set('contributor', account)
+      feature.set('likes', 1)
+      feature.set('isDisplayed', false)
+      feature.addUnique('supporters', account)
+      await feature.save()
+    }
+  }
+
+  return { approve }
+}
